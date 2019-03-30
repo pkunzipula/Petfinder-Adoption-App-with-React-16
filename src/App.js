5d8f80d902f8afcdfc528685e814be81adaea74f
pkunzipula <jcapra76@gmail.com>
2019-03-30 09:33:58 -0700

    Adding Prettier and ESLint to das Mix

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
 const Pet = props => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, props.name),
-            React.createElement("h2", {}, props.animal),
-            React.createElement("h2", {}, props.breed)
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
 };
 
 class App extends React.Component {
-    render() {
-        return React.createElement(
-            "div",
-            { className: "poopysville"},
-            [
-                React.createElement(Pet, {
-                    name: "Jay Bay",
-                    animal: "Dizzity Dog",
-                    breed: "Havartinese"
-                }),
-                React.createElement(Pet, {
-                    name: "Sawyer",
-                    anima: "Dogg",
-                    breed: "Mini Awesee"
-                }),
-                React.createElement(Pet, {
-                    name: "Dexter",
-                    animal: "Cat",
-                    breed: "Mean cat"
-                }),
-            ]
-        );
-    }
+  render() {
+    return React.createElement("div", { className: "poopysville" }, [
+      React.createElement(Pet, {
+        name: "Jay Bay",
+        animal: "Dizzity Dog",
+        breed: "Havartinese"
+      }),
+      React.createElement(Pet, {
+        name: "Sawyer",
+        anima: "Dogg",
+        breed: "Mini Awesee"
+      }),
+      React.createElement(Pet, {
+        name: "Dexter",
+        animal: "Cat",
+        breed: "Mean cat"
+      })
+    ]);
+  }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
