28e27aaaae6fd39ccd1cd0ec9570530df84bf2ea
jerry tumminia <TheClub@Club-Mac-Mini.local>
2019-04-02 11:31:27 -0700

    JSX

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+import pf from "petfinder-client";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ location: "Seattle, WA", output: "full" })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+        this.setState({
+          pets
+        });
+      });
+  }
   render() {
     return (
       <div>
         <h1>Adopt-a Mia!</h1>
-        <Pet name="Sawyer" animal="Dog" breed="Awesee" />
-        <Pet name="Dexipoo" animal="Cat" breed="Origin Unknown" />
-        <Pet name="Jaque Blaque" animal="Dog" breed="Weiner Pincher" />
+        {this.state.pets.map(pet => {
+          return (
+            <Pet
+              key={pet.id}
+              name={pet.name}
+              animal={pet.animal}
+              breed={pet.breed}
+              desc={pet.description}
+            />
+          )
+        })}
       </div>
-    );
+    )
   }
 }
 
